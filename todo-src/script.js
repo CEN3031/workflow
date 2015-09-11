0afc96f0980ce0853238ff081dae61987eff12e5
Francis Perez <fcsp19191993@ufl.edu>
2015-09-11 11:26:23 -0400

    Eliminated 'size' and simplified the AngularJS. Works perfectly.

--- todo-src/script.js
+++ todo-src/script.js
@@ -4,17 +4,14 @@ var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
-  $scope.completed = [false, false];
+  $scope.completed = [];
   $scope.newItem = "";
-  var size = $scope.todos.length;
 
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
       $scope.todos.push($scope.newItem);
       $scope.newItem = "";
-      $scope.completed[size] = false;
-      ++size;
     }
   };
     
@@ -23,28 +20,21 @@ myApp.controller('MainCtrl', function ($scope){
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
     $scope.completed.splice(index,1);
-    --size;
   };
     
   $scope.deleteCompletedItems = function() {
     console.log("in deletion of completed items");
-    var numDeleted = 0;
-    for(var i = size; i > -1; --i) {
-      console.log(i);
+    for(var i = $scope.todos.length; i > -1; --i) {
       if($scope.completed[i] == true) {
         $scope.todos.splice(i, 1);
         $scope.completed.splice(i,1);
-        ++numDeleted;
       }
     }
-    size -= numDeleted;
   };
 
   $scope.completeItem = function(item) {
     console.log("in completion");
-    var index = $scope.todos.indexOf(item);
-
-    $scope.completed[index] = true;
+    $scope.completed.push($scope.todos.indexOf(item));
   }
 });
 
