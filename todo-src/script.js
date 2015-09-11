4fee7121c1310e7286267890ca13bf5080a7af26
nick0210 <silvertrojan@ufl.edu>
2015-09-11 14:34:46 -0400

    Added edit button functionality

--- todo-src/script.js
+++ todo-src/script.js
@@ -2,35 +2,21 @@
 
 var myApp = angular.module('app', []);
 
-myApp.controller('MainCtrl', function ($scope){
+myApp.controller('MainCtrl', function($scope) {
   $scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
-  
-  $scope.addItem = function(){
+
+  $scope.addItem = function() {
     console.log("in add");
-    if ($scope.newItem !== ""){
+    if ($scope.newItem !== "") {
       $scope.todos.push($scope.newItem);
       $scope.newItem = "";
     }
-  }
-    
-  $scope.deleteItem = function(item){
+  };
+
+  $scope.deleteItem = function(item) {
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
-  }
-    
-  
+  };
 });
-
-/*************************
- * Homework (not rly):
- * - "enter" button functionality instead of clicking button
- * - edit button functionality
- * - button to mark item as "complete"
- * - have a total number of items at the top
- * - make it prettier
- * - add a due date
- * - add reminder (setInterval)
- * 
- * *********************/
