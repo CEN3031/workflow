7028d6781bd13d8525edbdb08a68ead069974aa1
woodmark888 <woodmark@ufl.edu>
2015-09-12 10:05:06 +0000

    Added Priority

--- todo-src/script.js
+++ todo-src/script.js
@@ -1,16 +1,48 @@
-// Code goes here
+/* global angular */
+/* global task */
+/* global priority */
 
+
+
+// Constructor is separate from its methods
+var Todo = function(task, priority) {
+    this.task = task;
+    this.priority = priority;
+}
+
+// Getters
+Todo.prototype.getTask = function() {
+    return task;
+};
+Todo.prototype.getPriority = function() {
+    return priority;
+};
+
+// Setters
+Todo.prototype.setTask = function(task) {
+    this.task = task;
+};
+
+Todo.prototype.setPriority = function(priority) {
+    this.priority = priority;
+};
+
+// App code starts here!
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
-  $scope.newItem = "";
+  $scope.todos = [new Todo("Learn Angular","HIGH"), new Todo("Learn node","HIGH")];
+  $scope.task = "";
+  $scope.priority = "";
   
   $scope.addItem = function(){
     console.log("in add");
-    if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
-      $scope.newItem = "";
+    if ($scope.task !== "" && $scope.priority !== ""){
+      var newItem = new Todo($scope.task,$scope.priority);
+      $scope.todos.push(newItem);
+      $scope.task = "";
+      $scope.priority = "";
+      
     }
   }
     
