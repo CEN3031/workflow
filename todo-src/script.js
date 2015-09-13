26392c82b1494c251c8fa2e8301e901ce90cbcfa
Xito12 <tonnyx1294@hotmail.com>
2015-09-13 18:35:04 -0400

    enter key function to add to list

--- todo-src/script.js
+++ todo-src/script.js
@@ -5,12 +5,40 @@ var myApp = angular.module('app', []);
 myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
+  $scope.priorityList = ["", ""];
+  $scope.priority = "";
+  $scope.total = 2;
   
+  $scope.keyPress = function($event) {
+    var key = $event.keyCode || $event.which;
+    if (key == 13) {
+      $scope.addItem();
+    }
+  }
+
+  $scope.setPriority = function(item){
+    console.log("in priority");
+    if ($scope.priority == "") {
+      $scope.priority = item;
+    }
+    $scope.addItem();
+  }
+
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
+      //places "now" task at top
+      if ($scope.priority == "now") { 
+        $scope.todos.unshift($scope.newItem);
+        $scope.priorityList.unshift($scope.priority);
+      }
+      else {
+        $scope.todos.push($scope.newItem);
+        $scope.priorityList.push($scope.priority);
+      }
       $scope.newItem = "";
+      $scope.priority = "";
+      $scope.total ++;
     }
   }
     
@@ -18,8 +46,9 @@ myApp.controller('MainCtrl', function ($scope){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
-  }
-    
+    $scope.priorityList.splice(index, 1);
+    $scope.total --;
+  } 
   
 });
 
