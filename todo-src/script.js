934f3bfdaee3bc2c668514e1ac6b5428f4092041
Jacob Logas <jakeman893@live.com>
2015-09-13 22:36:36 -0400

    Made the design object oriented to simplify task management. Check out the comments for explanations of the changes to the functions and uses of new functions.

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,31 +3,43 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function($scope) {
-	$scope.todos = ["Change background", "Priority", "Edit button", "Mark Complete", "Show number of the items", "Clear complete items", "Add enter"];
+	$scope.todos = [new Task("Change background"), new Task("Priority"), new Task("Edit button"), new Task("Mark Complete"), new Task("Show number of the items"), new Task("Clear complete items"), new Task("Add enter")];
 	$scope.newItem = "";
 	$scope.itemCount = $scope.todos.length;
 
-	$scope.addItem = function(){
+    //Adds a new task to the list
+	$scope.addTask = function(){
 		console.log("in add");
 		if ($scope.newItem !== ""){
-		  $scope.todos.push($scope.newItem);
+		  $scope.todos.push(new Task($scope.newItem));
 		  $scope.newItem = "";
 		  $scope.itemCount += 1;
 		}
 	}
 
+    //Deletes the items in the task list
 	$scope.deleteItem = function(item){
-		console.log("in delete");
-		var index = $scope.todos.indexOf(item);
-		$scope.todos.splice(index, 1);
+	    console.log("in delete");
+		$scope.todos.splice(item, 1);
 		$scope.itemCount -= 1;
 	}
 
+    //Allows for editing of the task's name
 	$scope.editItem = function (item) {
 		var check = prompt("Fix yo shit");
 		if (check != null) {
-			$scope.todos[item] = check;
+			$scope.todos[item].name = check;
 		} 
 	} 
 	
+    //The constructor/definition of task
+	function Task(name) {
+	    this.name = name;
+	    this.complete = false;
+	}
+
+    //Toggles the completed status of the task
+	Task.prototype.toggle = function () {
+	    this.complete = !this.complete;
+	}
 });
