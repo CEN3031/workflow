cec39be19efb3afba0f0acee7fff55125ca3ec3c
nate514 <nate@legum.us>
2015-09-15 13:10:45 -0400

    Complete

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,88 +3,30 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];   //list of items to do
-  $scope.completed = [];   //list of completed items
+  $scope.todos = ["Learn Angular", "Learn node"];
+  $scope.todid = [""];
   $scope.newItem = "";
-  $scope.priorityList = ["", ""];
-  $scope.priority = "";
-  $scope.total = 2;
-  $scope.completedItem = "";
   
-  $scope.setPriority = function(item){
-    console.log("in priority");
-    if ($scope.priority == "") {
-      $scope.priority = item;
-    }
-    $scope.addItem();
-  }
-
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
-      //places "now" task at top
-      if ($scope.priority == "now") { 
-        $scope.todos.unshift($scope.newItem);
-        $scope.priorityList.unshift($scope.priority);
-      }
-      else {
-        $scope.todos.push($scope.newItem);
-        $scope.priorityList.push($scope.priority);
-      }
+      $scope.todos.push($scope.newItem);
       $scope.newItem = "";
-      $scope.priority = "";
-      $scope.total ++;
     }
   }
-  
-  //should make deletion of list entry reusable code
+    
   $scope.deleteItem = function(item){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
-    $scope.priorityList.splice(index, 1);
-    $scope.total --;
   }
-
-  $scope.deleteCompletedItem = function(item){
-    console.log("in deleteCompletedItem ");
-    var index = $scope.completed.indexOf(item);
-    $scope.completed.splice(index, 1);
-  }
-
-  $scope.markComplete = function(item){
-    console.log("in markComplete");
-    
-    //grab the index of the item
+  
+  //want to take item and move it to stuff I did
+  $scope.completeItem = function(item){
+    console.log("in complete");
     var index = $scope.todos.indexOf(item);
-
-    //take the item and push an entry into the 'completed' list
-    $scope.completed.push($scope.todos[index]);
-
-    //remove the item from the todos list
+    $scope.todid.push($scope.todos[index]);
     $scope.todos.splice(index, 1);
-    $scope.priorityList.splice(index, 1);
-    $scope.total --;
+    index++;
   }
-
-  //adds item when "enter" is pressed
-  $scope.keyPress = function($event) {
-    var key = $event.keyCode || $event.which;
-    if (key == 13) {
-      $scope.addItem();
-    }
-  }
-
 });
-
-/*************************
- * Homework (not rly):
- * - "enter" button functionality instead of clicking button
- * - edit button functionality
- * - button to mark item as "complete"
- * - have a total number of items at the top
- * - make it prettier
- * - add a due date
- * - add reminder (setInterval)
- * 
- * *********************/
