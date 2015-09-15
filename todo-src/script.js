b4471c41b11a36931fdabe10f5f665b246c74a32
Madison Hicks <madleigh12@ufl.edu>
2015-09-14 21:50:01 -0400

    added priorities functionality

--- todo-src/script.js
+++ todo-src/script.js
@@ -1,36 +1,22 @@
 // Code goes here
 
 var myApp = angular.module('app', []);
-
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
-  $scope.newItem = "";
-  
+  $scope.todos = [{task: "Learn Angular", pri: "High"}, {task: "Learn node", pri: "Medium"}];
+  $scope.newItem = {
+        task: "",
+        pri: "",
+    };
   $scope.addItem = function(){
     console.log("in add");
-    if ($scope.newItem !== ""){
+    if ($scope.newItem.name !== "" && $scope.newItem.pri !== ""){
       $scope.todos.push($scope.newItem);
       $scope.newItem = "";
     }
-  }
-    
+  };
   $scope.deleteItem = function(item){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
-  }
-    
-  
+  };
 });
-
-/*************************
- * Homework (not rly):
- * - "enter" button functionality instead of clicking button
- * - edit button functionality
- * - button to mark item as "complete"
- * - have a total number of items at the top
- * - make it prettier
- * - add a due date
- * - add reminder (setInterval)
- * 
- * *********************/
