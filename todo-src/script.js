3622add80e54e823f1efbe227fbed17a57be14c4
Raz Aloni <raztheraz@gmail.com>
2015-09-15 00:16:30 -0400

    Completed tasks button plus  Clear Completed Tasks

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,13 +3,50 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
+  
+  $scope.todos =
+  [
+    { 
+      task  : "Learn Angular",
+      complete  : false
+    },
+    
+    {
+      task  : "Learn node",
+      complete  : true
+    },
+    
+    { 
+      task  : "3",
+      complete  : true
+    },
+    
+    { 
+      task  : "4",
+      complete  : false
+    },
+    
+    {
+      task  : "5",
+      complete  : true
+    },
+    
+    {
+      task  : "6",
+      complete  : false
+    }
+  ]
+  
   $scope.newItem = "";
   
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
+      $scope.todos.push(
+        {
+          task: $scope.newItem,
+          complete: false
+        });
       $scope.newItem = "";
     }
   }
@@ -20,6 +57,17 @@ myApp.controller('MainCtrl', function ($scope){
     $scope.todos.splice(index, 1);
   }
     
+  
+  $scope.removeCompleteItems = function()
+  {    
+    for (var i = $scope.todos.length-1; i >= 0; i--)
+    {
+        if($scope.todos[i].complete === true)
+        {
+            $scope.todos.splice(i, 1);
+        }
+    }
+  }
   
 });
 
