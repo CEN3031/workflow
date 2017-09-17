7d25d849d31d05b38a837683a40c64087ab08889
cristosilva <cristo.silva@yahoo.com>
2017-09-16 21:41:49 -0400

    Update script.js

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,7 +3,14 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
+  $scope.todos = [
+    {name: "Learn Angular", done: false, editting: false, priority: 3},
+    {name: "Learn node", done: false, editting: false, priority: 2},
+  //added this "test" to test
+  //added new field called priority
+    {name: "Test", done: false, editting: false, priority: 1}
+  ];
+
   $scope.newItem = "";
   
   $scope.addItem = function(){
@@ -11,16 +18,36 @@ myApp.controller('MainCtrl', function ($scope){
     if ($scope.newItem !== ""){
       $scope.todos.push($scope.newItem);
       $scope.newItem = "";
+  /* tried this to set priority level automatically to 3:
+  *   $scope.newItem.priority = 3;
+  * but i failed :(
+  */
     }
   }
-    
+  
   $scope.deleteItem = function(item){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
   }
+
+  $scope.done = function(item){
+    console.log("completed");
+    if(!item.editting) item.done = !item.done;
+  }
     
-  
+  $scope.editItem = function(item) {
+    if(!item.done) item.editting = !item.editting;
+  }
+// changePriority function. simply adds 1 to the priority
+// unless it is at max priority level, then it goes back to 1
+  $scope.changePriority = function(item) {
+    if(item.priority < 3)
+      item.priority = item.priority + 1;
+    else
+      item.priority = 1;
+  }
+
 });
 
 /*************************
