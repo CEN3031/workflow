a2ad641a7551bc06de75c65d87e8d6640fd34ac7
Vivian Garcia <garcviv@ufl.edu>
2019-10-28 14:10:05 -0400

    Changed todo names

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,7 +3,7 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
+  $scope.todos = ["Learning Angular", "Learning node"];
   $scope.newItem = "";
   
   $scope.addItem = function(){
